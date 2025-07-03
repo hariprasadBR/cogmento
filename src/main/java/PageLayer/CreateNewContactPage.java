94a7fb2cd686c52099ec7c987162595a3e76e3c7
hariprasad <hariprasadnomula7@gmail.com>
2025-07-03 15:41:52 +0530

    Hariprasad:Contactpageupdate

---
+++ src/main/java/PageLayer/CreateNewContactPage.java
@@ -0,0 +1,71 @@
+package PageLayer;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.PageFactory;
+
+import BaseLayerPackage.BaseClass;
+
+public class CreateNewContactPage extends BaseClass {
+
+	@FindBy(xpath = "//i[@class='users icon']")
+	private WebElement contactIcon;
+
+	@FindBy(xpath = "//button[text()='Create']")
+	private WebElement crateButton;
+
+	@FindBy(xpath = "//input[@name='first_name']")
+	private WebElement firstName;
+
+	@FindBy(xpath = "//input[@name='last_name']")
+	private WebElement lastName;
+
+	@FindBy(xpath = "//input[@name='value']")
+	private WebElement emailId;
+
+	@FindBy(xpath = "//label[text()='Status']/following-sibling::div/child::i[@class='dropdown icon']")
+	private WebElement status;
+
+	@FindBys(@FindBy(xpath = "//div[@name='status']/child::span"))
+	private List<WebElement> statusList;
+
+	@FindBy(xpath = "//button[text()='Save']")
+	private WebElement saveButton;
+
+	public CreateNewContactPage() {
+		PageFactory.initElements(getDriver(), this);
+	}
+
+	public void createNewContactFuncationality(String firstName, String lastName, String emailId, String exptValue)
+			throws InterruptedException {
+		Thread.sleep(4000);
+		contactIcon.click();
+		crateButton.click();
+		this.firstName.sendKeys(firstName);
+		this.lastName.sendKeys(lastName);
+		this.emailId.sendKeys(emailId);
+		this.status.click();
+
+		Thread.sleep(4000);
+
+		for (Iterator<WebElement> iterator = statusList.iterator(); iterator.hasNext();) {
+			WebElement webelement = (WebElement) iterator.next();
+			String actualText = ((WebElement) iterator.next()).getText();
+
+			if (actualText.equals(exptValue)) {
+				webelement.click();
+				break;
+			}
+
+		}
+
+		Thread.sleep(4000);
+		saveButton.click();
+
+	}
+
+}
--- src/test/java/TestLayer/CloginTestcases.java
+++ src/test/java/TestLayer/CloginTestcases.java
@@ -1,12 +1,17 @@
 package TestLayer;
 
+import java.io.IOException;
+
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import BaseLayerPackage.BaseClass;
 import PageLayer.CloginPage;
+import PageLayer.CreateNewContactPage;
+import Reader.ExelReader;
 
 @Listeners(ListenersLayer.Reports.class)
 public class CloginTestcases extends BaseClass {
@@ -24,5 +29,31 @@ public class CloginTestcases extends BaseClass {
 //		prafulp1010@gmail.com
 //		Pr@ful0812
 	}
+	
+	@Test(dataProvider="cogmentoData",dependsOnMethods = {"validateLoginFuncationality"} )
+	public void validateCreateNewContactFuncationality(String firstName, String lastName, String emailId, String exptValue) throws InterruptedException {
+		
+		CreateNewContactPage create = new CreateNewContactPage();
+		create.createNewContactFuncationality(firstName, lastName, emailId, exptValue);
+	}
+	
+	@DataProvider(name="cogmentoData")
+	public Object[][] dataReader() {
+		Object[][] data = null;
+
+	    try {
+	        ExelReader reader = new ExelReader("cogmentoTest.xlsx", 0);
+	        data = reader.getAllData();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	        // Optionally return empty data to prevent TestNG from crashing
+	        return new Object[0][0];
+	    }
+
+	    return data;
+		
+	}
+	
+
 
 }
