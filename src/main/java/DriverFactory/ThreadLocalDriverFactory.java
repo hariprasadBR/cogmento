c6473ed8f211a8adc76f5349c71ff00dca86dfb3
Nomula Hariprasad <84274712+hariprasadBR@users.noreply.github.com>
2026-02-03 07:08:48 +0530

    Update ThreadLocalDriverFactory with grid

--- src/main/java/DriverFactory/ThreadLocalDriverFactory.java
+++ src/main/java/DriverFactory/ThreadLocalDriverFactory.java
@@ -7,26 +7,28 @@ import org.openqa.selenium.WebDriver;
 import HelperLayer.BrowserManager;
 
 public class ThreadLocalDriverFactory {
-	private static final Logger logger = LogManager.getLogger(ThreadLocalDriverFactory.class);
- 
-	private static ThreadLocal<WebDriver> tl = new ThreadLocal<WebDriver>();
 
-	public static WebDriver getDriver() {
-		logger.info("Instance getting from threadlocal");
-		return tl.get();
-	}
-	
-	public static void setDriver(String browsername) {
-		logger.info("Instance setting from threadlocal");
-		BrowserManager driverManager=DriverFactory.getBroswer(browsername);
-		tl.set(driverManager.browserinitialization());
-		
-	}
-	public static void quitDriver() {
-    if (tl.get() != null) {
-        tl.get().quit();   // closes browser
-        tl.remove();       // removes thread reference ✅
-        logger.info("Driver quit and ThreadLocal cleaned");
+    private static final Logger logger =
+            LogManager.getLogger(ThreadLocalDriverFactory.class);
+
+    private static ThreadLocal<WebDriver> tl = new ThreadLocal<>();
+
+    public static WebDriver getDriver() {
+        logger.info("Getting driver from ThreadLocal");
+        return tl.get();
     }
-}
+
+    public static void setDriver(String browserName) {
+        logger.info("Setting driver in ThreadLocal");
+        BrowserManager manager = DriverFactory.getBrowser(browserName);
+        tl.set(manager.initializeBrowser(browserName));
+    }
+
+    public static void quitDriver() {
+        if (tl.get() != null) {
+            tl.get().quit();
+            tl.remove();
+            logger.info("Driver quit & ThreadLocal cleaned");
+        }
+    }
 }
